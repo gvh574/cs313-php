a387acc4fca5c400e1be52304eab1b6765701f4e
Gregory Hall <Greg@Gregorys-MacBook-Pro.local>
2016-02-23 13:38:47 -0700

    changed db connection

--- phpApp/model/db.php
+++ phpApp/model/db.php
@@ -23,14 +23,15 @@ else
 // In the openshift environment
 //echo "Using openshift credentials: ";
 
+$dbName = 'housing';
 $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+$dbPort = ':'.getenv('OPENSHIFT_MYSQL_DB_PORT'); 
 $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 } 
     
 try {
-$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+$db = new PDO("mysql:host=$dbHost$dbPort;dbname=$dbName", $dbUser, $dbPassword);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (Exception $ex) {
